7425acd3805ac53da0ef0fa498ccb19ad4fcd9c5
Mahmoud Elshekh Ali <109414725+Ionvak@users.noreply.github.com>
2025-04-16 21:22:54 +0200

    Initializing revamped project for task 2
    There were some issues with the project from task 1 that require it to be completely revamped for convenience. The main one being lack of access to Visual studio and scaffolding tools.

---
+++ lab2/TripPlanner/Models/Trip.cs
@@ -0,0 +1,13 @@
+﻿namespace TripPlanner.Models
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Capacity { get; set; }
+        public string Date { get; set; }
+        public int OwnerId { get; set; }
+        public List<User> Users { get; set; }
+    }
+}
