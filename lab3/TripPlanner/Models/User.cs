05fab8ad9146d59cce61cc180ff326740d531d39
Mahmoud Elshekh Ali <109414725+Ionvak@users.noreply.github.com>
2025-05-19 22:52:13 +0200

    Initialization of ASP.NET core webAPI REACT project in Visual Studio

---
+++ lab3/TripPlanner/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TripPlanner.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public ICollection<Trip>? Trips { get; set; }
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        
+    }
+}
